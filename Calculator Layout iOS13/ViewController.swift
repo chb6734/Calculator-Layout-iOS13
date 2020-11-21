5142f6d1ae89fa5377ff18e4ac35935bf458e3b8
aofhdzzz1 <chb6734@gmail.com>
2020-11-22 02:37:21 +0900

    변수 선언 및 버튼 클릭이벤트 생성

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -14,7 +14,61 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var resultValue = 0
+    var displayValue = 0
+    
+    
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    
 
-
+    // Number
+    @IBAction func btn0_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn1_Clcik(_ sender: UIButton) {
+    }
+    @IBAction func btn2_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn3_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn4_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn5_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn6_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn7_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn8_Click(_ sender: UIButton) {
+    }
+    @IBAction func btn9_Click(_ sender: UIButton) {
+    }
+    
+    // Number Sentence
+    @IBAction func btnDiv_Click(_ sender: UIButton) {
+    }
+    @IBAction func btnMul_Click(_ sender: UIButton) {
+    }
+    @IBAction func btnMin_Click(_ sender: UIButton) {
+    }
+    @IBAction func btnPlus_Click(_ sender: UIButton) {
+    }
+    @IBAction func btnEqul_Click(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnRemainder_Click(_ sender: UIButton) {
+    }
+    @IBAction func btnSign_Click(_ sender: UIButton) {
+    }
+    @IBAction func btnAC_Click(_ sender: UIButton) {
+    }
 }
 
